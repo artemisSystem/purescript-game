df79a5b4991fbadb40f1b3bb6945fc107e4ca933
3ddy <eivind@hukkelberg.net>
2020-04-24 22:53:56 +0200

    boilerplate for event test

---
+++ test/Test/Event/Main.purs
@@ -0,0 +1,27 @@
+module Test.Event.Main where
+
+import Prelude
+
+import Data.Traversable
+import Data.Maybe
+import Effect
+import Effect.Aff
+import Effect.Class.Console
+import Effect.Exception
+import Game
+import Game.Util
+import Game.Aff
+import Game.Aff.Web
+import Game.Aff.Web.Util
+import Game.Aff.Web.Event
+import Run.Reader
+import Run.State
+import Web.HTML
+import Web.HTML.Window
+import Web.HTML.HTMLDocument
+import Web.HTML.HTMLInputElement
+import Web.HTML.HTMLInputElement
+import Web.DOM.ParentNode
+import Web.Event.Event
+import Web.Event.EventTarget
+
